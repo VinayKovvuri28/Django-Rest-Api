3a4bdc1651f80d1df492355a58dda4918c3eb2ed
VinayKovvuri28 <vinaykovvuri28@gmail.com>
2023-12-12 19:35:26 +0530

    ok

--- tutorial/student/views.py
+++ tutorial/student/views.py
@@ -3,8 +3,9 @@ from .models import StudentModel
 from .serializers import StudentModelSerializers
 from rest_framework import status, viewsets
 from rest_framework.response import Response
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated 
+# Local Authentications and Permissions import
+# from rest_framework.authentication import BasicAuthentication
+# from rest_framework.permissions import IsAuthenticated 
 
 # Create your views here.
 class Studentviewset(viewsets.ModelViewSet):
